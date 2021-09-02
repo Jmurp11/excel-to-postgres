2de7434084eff6431e766b2a8f02a27c47d56123
Jim Murphy <murphyj1011@gmail.com>
2021-09-02 09:18:01 -0400

    fix: add export excelToPostgresdb

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,1 @@
-import { excelToPostgresDb } from './sql';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
-
-excelToPostgresDb({
-	host: 'localhost',
-	user: 'postgres',
-	password: 'postgres',
-	database: 'test',
-	port: 5432
-}, path.resolve(__dirname, './data/2021_Projections.xlsx'),
-{
-	createDatabase: true,
-	createTables: true
-});
+export { excelToPostgresDb } from './sql';
