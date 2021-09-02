1ad7c0771f421aa4314e74a31968c60b9268d5d8
Jim Murphy <murphyj1011@gmail.com>
2021-09-01 20:22:58 -0400

    test: update the property name to include a space

--- src/tests/etl-processes.test.ts
+++ src/tests/etl-processes.test.ts
@@ -1,15 +1,16 @@
 import { getFields, getColumns, formatColumns } from '../etl-processes';
 import { columns, etlProcesses } from './test-data';
+
 describe('ETL processes tests', () => {
-    test('getFields', () => {
-        expect(getFields(etlProcesses.sheet)).toEqual(etlProcesses.fields);
-    });
+	test('getFields', () => {
+		expect(getFields(etlProcesses.sheet)).toEqual(etlProcesses.fields);
+	});
 
-    test('getColumns', () => {
-        expect(getColumns(etlProcesses.fields)).toEqual(columns);
-    });
+	test('getColumns', () => {
+		expect(getColumns(etlProcesses.fields)).toEqual(columns);
+	});
 
-    test('formatColumns', () => {
-        expect(formatColumns(columns)).toEqual(etlProcesses.formattedColumns);
-    });
+	test('formatColumns', () => {
+		expect(formatColumns(columns)).toEqual(etlProcesses.formattedColumns);
+	});
 });
--- src/tests/test-data.ts
+++ src/tests/test-data.ts
@@ -1,58 +1,58 @@
 const mockJSON = {
-    name: 'Person 1',
-    age: 18,
-    isDev: true
+	'person name': 'Person 1',
+	age: 18,
+	isDev: true
 };
 
 export const columns = [
-    {
-        name: 'name',
-        type: 'VARCHAR'
-    },
-    {
-        name: 'age',
-        type: 'FLOAT'
-    },
-    {
-        name: 'isDev',
-        type: 'BOOLEAN'
-    }
+	{
+		name: 'name',
+		type: 'VARCHAR'
+	},
+	{
+		name: 'age',
+		type: 'FLOAT'
+	},
+	{
+		name: 'isDev',
+		type: 'BOOLEAN'
+	}
 ];
 
 export const etlProcesses = {
-    sheet: {
-        name: mockJSON.name,
-        age: mockJSON.age,
-        isDev: mockJSON.isDev
-    },
-    fields: {
-        names: [
-            'name',
-            'age',
-            'isDev'
-        ],
-        values: [
-            mockJSON.name,
-            mockJSON.age,
-            mockJSON.isDev
-        ]
-    },
-    formattedColumns: [
-        `${columns[0].name} ${columns[0].type}`,
-        `${columns[1].name} ${columns[1].type}`,
-        `${columns[2].name} ${columns[2].type}`
-    ],
+	sheet: {
+		name: 'Person 1',
+		age: mockJSON.age,
+		isDev: mockJSON.isDev
+	},
+	fields: {
+		names: [
+			'name',
+			'age',
+			'isDev'
+		],
+		values: [
+			'Person 1',
+			mockJSON.age,
+			mockJSON.isDev
+		]
+	},
+	formattedColumns: [
+		`${columns[0].name} ${columns[0].type}`,
+		`${columns[1].name} ${columns[1].type}`,
+		`${columns[2].name} ${columns[2].type}`
+	],
 };
 
 export const sqlInfo = {
-    database: 'database',
-    tableName: 'tableName'
+	database: 'database',
+	tableName: 'tableName'
 };
 
 export const sqlResults = {
-    createDatabase: `CREATE DATABASE ${sqlInfo.database};`,
-    createTable: `CREATE TABLE ${sqlInfo.tableName} (
+	createDatabase: `CREATE DATABASE ${sqlInfo.database};`,
+	createTable: `CREATE TABLE ${sqlInfo.tableName} (
         ${etlProcesses.formattedColumns}
     );`,
-    insert: `INSERT INTO ${sqlInfo.tableName.replace(/\s/g, '')}(name,age,isDev) VALUES ('${mockJSON.name}',${mockJSON.age},${mockJSON.isDev});`
-}
+	insert: `INSERT INTO ${sqlInfo.tableName.replace(/\s/g, '')}(name,age,isDev) VALUES ('Person 1',${mockJSON.age},${mockJSON.isDev});`
+};
