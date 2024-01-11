c6a9ca7b92fdd95da04c0d1c07422027d288172a
Jim-murphy_mmcgh <jim.murphy@nera.com>
2024-01-11 16:53:41 -0500

    start error-handling branch

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { excelToPostgresDb } from "./sql";
+import { excelToPostgresDb } from './sql';
 
 excelToPostgresDb(
-  {
-    host: "localhost",
-    user: "postgres",
-    password: "postgres",
-    port: 5432,
-    database: "baseballdb",
-  },
-  "/Users/jim.murphy/Documents/workspace/multi-sport-draft-aid/server/data/baseballdb.xlsx",
-  {
-    createTables: true,
-  }
+	{
+		host: 'localhost',
+		user: 'postgres',
+		password: 'postgres',
+		port: 5432,
+		database: 'baseballdb',
+	},
+	'/Users/jim.murphy/Documents/workspace/multi-sport-draft-aid/server/data/baseballdb.xlsx',
+	{
+		createTables: true,
+	}
 ).then((t) => console.log(t));
